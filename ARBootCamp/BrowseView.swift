8666596d82b6a6dc5d7ad93830af676ba7b9f169
Arsalan Wahid Asghar <arsalanwahid1993@gmail.com>
2024-10-06 16:53:10 +0500

    [Feature] implements horizontal Lazy Gird for browse view

--- ARBootCamp/BrowseView.swift
+++ ARBootCamp/BrowseView.swift
@@ -15,7 +15,7 @@ struct BrowseView: View {
     var body: some View {
         NavigationStack {
             ScrollView(showsIndicators: false) {
-                
+                HorizaontalGrid()
             }.navigationTitle("Browse")
                 .navigationBarItems(trailing: Button(action: {
                     self.showBrowse.toggle()
@@ -24,4 +24,31 @@ struct BrowseView: View {
                 })
         }
     }
+}
+
+struct HorizaontalGrid:View {
+    private let gridItemLayout = [GridItem(.fixed(150))]
+    
+    var body: some View {
+        VStack(alignment:.leading) {
+            Text("category Title")
+                .font(.title2).bold()
+                .padding(.leading,20)
+                .padding(.top,10)
+            ScrollView {
+                LazyHGrid(rows: gridItemLayout, spacing: 30) {
+                    ForEach(0..<5) { index in
+                        Color(UIColor.secondarySystemFill)
+                            .frame(width: 150,height: 150).cornerRadius(8)
+                        
+                    }
+                }.padding(.horizontal,22)
+                    .padding(.vertical,10)
+            }
+            
+            
+        }
+    }
+    
+    
 }
